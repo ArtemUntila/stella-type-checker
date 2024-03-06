0b34ea3e24c52b1d943d60a0b8592c5fa6edec2a
ArtemUntila <untila_artem@mail.ru>
2024-03-06 16:52:26 +0300

    Init project + Antlr

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+    antlr
+}
+
+group = "artem.untila"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    antlr("org.antlr:antlr4:4.13.1")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(11)
+}
+
+tasks {
+    generateGrammarSource {
+        arguments = arguments + listOf("-visitor")
+    }
+
+    compileKotlin {
+        dependsOn("generateGrammarSource")
+    }
+
+    compileTestKotlin {
+        dependsOn("generateTestGrammarSource")
+    }
+}
