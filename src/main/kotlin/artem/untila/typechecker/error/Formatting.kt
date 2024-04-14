7a6790aa88c0cfe45f7238387f48e548e927a9a1
ArtemUntila <untila_artem@mail.ru>
2024-04-14 14:04:40 +0300

    Simplify `Formatting`

--- src/main/kotlin/artem/untila/typechecker/error/Formatting.kt
+++ src/main/kotlin/artem/untila/typechecker/error/Formatting.kt
@@ -1,47 +1,11 @@
 package artem.untila.typechecker.error
 
-import org.antlr.v4.runtime.tree.*
+import org.antlr.v4.runtime.ParserRuleContext
+import org.antlr.v4.runtime.misc.Interval
 
 // Source code formatting
-val ParseTree.src: String
-    get() = Formatter.format(this)
-
-object Formatter : AbstractParseTreeVisitor<String>() {
-
-    private lateinit var sb: StringBuilder
-
-    fun format(tree: ParseTree): String {
-        sb = StringBuilder()
-        tree.accept(this)
-        return sb.toString()
-    }
-
-    private val last: Char?
-        get() = sb.lastOrNull()
-
-    override fun visitTerminal(node: TerminalNode): String {
-        return when (val s = node.text) {
-            "as" -> " as "
-            "," -> ", "
-            ":" -> " : "
-            "{" -> if (last != null && last != ' ') " { " else "{ "
-            "}" -> " }"
-            "=" -> " = "
-            "if" -> "if "
-            "then" -> " then "
-            "else" -> " else "
-            "return" -> "return "
-            "match" -> "match "
-            "<|" -> "<| "
-            "|>" -> " |>"
-            "|" -> " | "
-            "=>" -> " => "
-            ":=" -> " := "
-            ";" -> "; "
-            else -> s
-        }.also { sb.append(it) }
-    }
-}
+val ParserRuleContext.src: String
+    get() = start.inputStream.getText(Interval.of(start.startIndex, stop.stopIndex))
 
 // Error report formatting
 private const val tab = "  "
